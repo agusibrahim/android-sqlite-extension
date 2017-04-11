a8b24263575b50fe195d2fbc03b7415a03638725
root <root@ip-172-31-17-194.ap-southeast-1.compute.internal>
2017-04-11 00:50:59 +0000

    move to folder

--- build.sh
+++ build.sh
@@ -5,17 +5,26 @@ CCX86_64=/opt/ndk/std-toolchains/android-21-x86_64/bin/gcc
 CCMIPS=/opt/ndk/std-toolchains/android-9-mips/bin/gcc
 CCMIPS64=/opt/ndk/std-toolchains/android-21-mips64/bin/gcc
 DBU=$(pwd)/build
+DIRARM=$DBU/arm
+DIRARM64=$DBU/arm64
+DIRX86=$DBU/x86
+DIRX86_64=$DBU/x86_64
+DIRMIPS=$DBU/mips
+DIRMIPS64=$DBU/mips64
+rm -rf $DBU
+mkdir $DBU
+mkdir $DIRARM $DIRX86 $DIRARM64 $DIRX86_64 $DIRMIPS $DIRMIPS64
 
 for i in "damerau_levenshtein" "levenshtein" "soundex" "compress" "spellfix" "fuzzer" "nextchar" "percentile" "amatch" "sha1" "fileio" "closure" "scrub" "regexp" "csv" "json1" "shathree" "vtshim" "wholenumber" "rot13" "carray" "series" "remember" "liblevenshtein" "metaphone"
 do
-  $CCX86 -shared -fPIC -Wall -I/agus/sqlite-amalgamation-3180000/SQLiteFuzzySearch $i.c -o $DBU/$i"_x86.so"
-  $CCARM -shared -fPIC -Wall -I/agus/sqlite-amalgamation-3180000/SQLiteFuzzySearch $i.c -o $DBU/$i"_arm.so"
+  $CCX86 -shared -fPIC -Wall -I$(pwd) $i.c -o $DIRX86/$i".so"
+  $CCARM -shared -fPIC -Wall -I$(pwd) $i.c -o $DIRARM/$i".so"
 
-  $CCARM64 -shared -fPIC -Wall -I/agus/sqlite-amalgamation-3180000/SQLiteFuzzySearch $i.c -o $DBU/$i"_arm64.so"
-  $CCX86_64 -shared -fPIC -Wall -I/agus/sqlite-amalgamation-3180000/SQLiteFuzzySearch $i.c -o $DBU/$i"_x86-64.so"
+  $CCARM64 -shared -fPIC -Wall -I$(pwd) $i.c -o $DIRARM64/$i".so"
+  $CCX86_64 -shared -fPIC -Wall -I$(pwd) $i.c -o $DIRX86_64/$i".so"
 
-  $CCMIPS -shared -fPIC -Wall -I/agus/sqlite-amalgamation-3180000/SQLiteFuzzySearch $i.c -o $DBU/$i"_mips.so"
-  $CCMIPS64 -shared -fPIC -Wall -I/agus/sqlite-amalgamation-3180000/SQLiteFuzzySearch $i.c -o $DBU/$i"_mips64.so"
+  $CCMIPS -shared -fPIC -Wall -I$(pwd) $i.c -o $DIRMIPS/$i".so"
+  $CCMIPS64 -shared -fPIC -Wall -I$(pwd) $i.c -o $DIRMIPS64/$i".so"
 
   echo  $i.so
 done;
